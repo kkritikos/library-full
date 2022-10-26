7d4faaefc4c375e141b66438c4785502de60c48c
Kyriakos Kritikos <domcretan@gmail.com>
2022-10-26 10:56:09 +0300

    Changed scripts in order to comply to the three phases of the container-based application management process

---
+++ setup.sh
@@ -0,0 +1,2 @@
+sudo docker build -t service:latest .
+sudo docker build -t db:latest -f Dockerfile_mysql .
--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
-sudo docker build -t service:latest .
-sudo docker build -t db:latest -f Dockerfile_mysql .
 sudo docker network create net
 sudo docker run -d -v /home/ubuntu/mysql:/var/lib/mysql --network net --name mysql db:latest
 sudo docker run -d -p 8080:8080 --network net --name service service:latest
