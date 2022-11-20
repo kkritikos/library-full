210fa94223b457f9f7a508fe723608ca65a0893a
Kyriakos Kritikos <domcretan@gmail.com>
2022-11-20 22:27:52 +0200

    small update to start.sh script to include random root pws option/environment variable

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 sudo docker network create net
-sudo docker run -d -v /home/ubuntu/mysql:/var/lib/mysql --network net --name mysql db:latest
+sudo docker run -d -v /home/ubuntu/mysql:/var/lib/mysql -e MYSQL_RANDOM_ROOT_PASSWORD=yes --network net --name mysql db:latest
 sudo docker run -d -p 8080:8080 --network net --name service service:latest
