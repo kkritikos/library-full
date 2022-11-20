820b57d688f14e423351f0e81c8a8930bbd5c6da
Kyriakos Kritikos <domcretan@gmail.com>
2022-11-20 22:50:37 +0200

    changes to allow correct insertion of setup.sql file

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 sudo docker network create net
-sudo docker run -d -v /home/ubuntu/mysql:/var/lib/mysql -v "$(pwd)"/setup.sql:/docker-entrypoint-initdb.d -e MYSQL_RANDOM_ROOT_PASSWORD=yes --network net --name mysql mysql
+sudo docker run -d -v /home/ubuntu/mysql:/var/lib/mysql -v "$(pwd)"/mysql:/docker-entrypoint-initdb.d -e MYSQL_RANDOM_ROOT_PASSWORD=yes --network net --name mysql mysql
 sudo docker run -d -p 8080:8080 --network net --name service service:latest
