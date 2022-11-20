8fd1184263f5efde8fa9f86e07373d30e0aabdb9
Kyriakos Kritikos <domcretan@gmail.com>
2022-11-20 22:35:39 +0200

    adding setup.sql as startup script of normal mysql image

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 sudo docker network create net
-sudo docker run -d -v /home/ubuntu/mysql:/var/lib/mysql -e MYSQL_RANDOM_ROOT_PASSWORD=yes --network net --name mysql db:latest
+sudo docker run -d -v /home/ubuntu/mysql:/var/lib/mysql -v ./setup.sql:/docker-entrypoint-initdb.d -e MYSQL_RANDOM_ROOT_PASSWORD=yes --network net --name mysql mysql
 sudo docker run -d -p 8080:8080 --network net --name service service:latest
